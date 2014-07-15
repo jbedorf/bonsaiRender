41afeaf56004a34570153ec28344d719a6a8323c
Evghenii <egaburov@dds.nl>
2014-07-15 13:45:14 +0200

    =1

--- render/main.cpp
+++ render/main.cpp
@@ -2,6 +2,9 @@
 #include <cstdlib>
 #include <cassert>
 #include <unistd.h>
+#include <mpi.h>
+#include <sstream>
+#include "read_tipsy.h"
 
 #include "renderloop.h"
 #include "anyoption.h"
@@ -9,8 +12,74 @@
 
 int main(int argc, char * argv[])
 {
-  std::string fullScreenMode;
-  bool stereo = false;
+  MPI_Comm comm = MPI_COMM_WORLD;
+  MPI_Init(&argc, &argv);
+    
+  int nranks, rank;
+  MPI_Comm_size(comm, &nranks);
+  MPI_Comm_rank(comm, &rank);
+
+  assert(nranks == 1);
+  assert(rank   == 0);
+
+  std::string fileName;
+  int nDomains     = -1;
+  int reduceFactor =  1;
+  std::string fullScreenMode    = "";
+  bool stereo     = false;
+
+
+  if (rank == 0)  
+  {
+		AnyOption opt;
+
+#define ADDUSAGE(line) {{std::stringstream oss; oss << line; opt.addUsage(oss.str());}}
+
+		ADDUSAGE(" ");
+		ADDUSAGE("Usage:");
+		ADDUSAGE(" ");
+		ADDUSAGE(" -h  --help             Prints this help ");
+		ADDUSAGE(" -i  --infile #         Input snapshot filename ");
+    ADDUSAGE(" -n  --ndomains #       Number of domains ");
+		ADDUSAGE(" -r  --reducefactor #   cut down bodies dataset by # factor [1])")
+		ADDUSAGE("     --fullscreen #     set fullscreen mode string");
+		ADDUSAGE("     --stereo           enable stereo rendering");
+		ADDUSAGE(" ");
+
+
+		opt.setFlag  ( "help" ,        'h');
+		opt.setOption( "infile",       'i');
+		opt.setOption( "ndomains",     'n');
+		opt.setOption( "reducefactor", 'r');
+    opt.setOption( "fullscreen");
+    opt.setFlag("stereo");
+
+    opt.processCommandArgs( argc, argv );
+
+
+    if( ! opt.hasOptions() ||  opt.getFlag( "help" ) || opt.getFlag( 'h' ) )
+    {
+      /* print usage if no options or requested help */
+      opt.printUsage();
+      ::exit(0);
+    }
+
+    char *optarg = NULL;
+    if ((optarg = opt.getValue("infile")))       fileName           = std::string(optarg);
+    if ((optarg = opt.getValue("ndomains")))     nDomains           = atoi(optarg);
+    if ((optarg = opt.getValue("reducefactor"))) reduceFactor       = atoi(optarg);
+    if ((optarg = opt.getValue("fullscreen")))	 fullScreenMode     = std::string(optarg);
+    if (opt.getFlag("stereo"))  stereo = true;
+
+    if (fileName.empty() || nDomains == -1 || reduceFactor < 1)
+    {
+      opt.printUsage();
+      ::exit(0);
+    }
+
+#undef ADDUSAGE
+  }
+
   initGL(argc, argv, fullScreenMode.c_str(), stereo);  
   fprintf(stderr, " -- Done -- \n");
   sleep(1000);
