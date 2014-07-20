a79af38cf7ae369feee57b469aeb90452e891a5d
evghenii <egaburov@dds.nl>
2014-07-20 19:30:36 +0200

    i think ig ot some working ideas

--- IO/BonsaiIO.cpp
+++ IO/BonsaiIO.cpp
@@ -1,4 +1,217 @@
 #include "BonsaiIO.h"
+  
+class IDType;
+{
+  public:
+    uint64_t ID;
+  private:
+    uint64_t getID() const
+    {
+      return _IDTypePacked & 0xFFFF000000000000ULL;
+    }
+    uint32_t getType() const
+    {
+      return static_cast<uint32_t>(_IDTypePacked >> 48);
+    }
+    void setID(const int64_t ID)
+    {
+      const uint32_t type = getType();
+      _IDTypePacked = (ID & 0xFFFF000000000000ULL) | (static_cast<uint64_t>(type) << 48);
+    }
+    void setType(const int type)
+    {
+      const uint64_t ID = getID();
+      _IDTypePacked  = ID | (static_cast<uint64_t>(type) << 48);
+    }
+};
+
+void writeSnapshot(
+    real4 *bodyPositions,
+    real4 *bodyVelocities,
+    uulong* bodiesIDs,
+    const int n,
+    const int nDomains,
+    const std::string &fileName,
+    const float time,
+    const MPIComm &comm,
+    const int nRank, const int myRank)
+{
+  BonsaiIO::Core out(myRank, nRank, comm, BonsaiIO::WRITEMPI, fileName);
+
+  out.setNDomains(nDomains);
+
+  /* write IDs */
+  {
+    BonsaiIO::DataType<IDType> ID("IDType", n);
+#pragma omp parallel for
+    for (int i = 0; i < n; i++)
+    {
+      ID[i].setID(bodiesIDs[i]);
+      int type = 0;
+      if(bodyIds[i] >= DISKID  && bodyIds[i] < BULGEID)       type = 2;
+      if(bodyIds[i] >= BULGEID && bodyIds[i] < DARKMATTERID)  type = 1;
+      if(bodyIds[i] >= DARKMATTERID)                          type = 0;
+      ID[i].setType(type);
+    }
+    out.write(ID);
+  }
+
+  /* write pos */
+  {
+    BonsaiIO::DataType<real4> pos("POS",n);
+#pragma omp parallel for
+    for (int i = 0; i < n; i++)
+      pos[i] = bodyPositions[i];
+    out.write(pos);
+  }
+
+  /* write velocities */
+  {
+    typedef float[3] vec3;
+    BonsaiIO::Vector<vec3> vel("VEL",n);
+#pragma omp parallel for
+    for (int i = 0; i < n; i++)
+    {
+      vel[i][0] = bodyVelocities[i].x;
+      vel[i][1] = bodyVelocities[i].y;
+      vel[i][2] = bodyVelocities[i].z;
+    }
+    out.write(vel);
+  }
+  
+  /* write rhoh */
+  {
+    typedef float[2] vec2;
+    BonsaiIO::DataType<vec2> rhoh("RHOH",n);
+#pragma omp parallel for
+    for (int i = 0; i < n; i++)
+    {
+      rhoh[i][0] = 0; /* rho */
+      rhoh[i][1] = 0; /*  h  */
+    }
+    out.write(rhoh);
+  }
+
+  out.close();
+}
+
+void readSnapshot(
+    std::vector<real4>  &bodyPositions,
+    std::vector<real4>  &bodyVelocities,
+    std::vector<uulong> &bodiesID,
+    std::vector<real2>  &rhohList,
+    const std::string   &fileName,
+    const MPIComm       &comm,
+    const int nRank, 
+    const int myRank,
+    int &NTotal2,
+    int &NFirst, int &NSecond, int &NThird,
+    std::vector<real4> &dustPositions, std::vector<real4> &dustVelocities,
+    std::vector<ullong> &dustIDs, 
+    const int reduce_bodies_factor,
+    const int reduce_dust_factor,
+    const bool restart)
+{
+  NFirst = NSecond = NThird = 0;
+  BonsaiIO::Core out(fileName, BonsaiIO::READMPI);
+
+  {
+    BonsaiIO::DataType<IDType> IDList("IDType");
+    if (!out.read(IDList))
+    {
+      if (myRank == 0)
+        fprintf(stderr, " FATAL: No particle ID data is found. Please make sure you passed the right file \n");
+      exit(-1);
+    }
+    const int n = IDList.size();
+    bodiesID.resize(n);
+#pragma omp parallel for
+    for (int i = 0; i < n; i++)
+    {
+      bodiesID[i] = IDList[i].getID();
+      switch (IDList[i].getType())
+      {
+        case 0:
+          NFirst++;
+          break;
+        case 1:
+          NSecond++;
+          break;
+        case 2:
+          NThird++;
+          break;
+      }
+    }
+    NTotal2 = NFirst+NSecond+NThird;
+    assert(NTotal2 == n);
+  }
+
+  {
+    BonsaiIO::DataType<real4> pos("pos");
+    if (!out.read(pos))
+    {
+      if (myRank == 0)
+        fprintf(stderr, " FATAL: No particle positions data is found. Please make sure you passed the right file \n");
+      exit(-1);
+    }
+    const int n = pos.size();
+    bodyPositions.resize(n);
+    assert(bodyPositions.size() == bodiesID.size());
+#pragma omp parallel for
+    for (int i = 0; i < n; i++)
+      bodyPositions[i] = pos[i];
+  }
+
+  {
+    typedef float[3] vec3;
+    Bonsai::DataType<vec3> vel("VEL");
+    if (!out.read(vel))
+    {
+      if (myRank == 0)
+        fprintf(stderr, " FATAL: No particle velocity data is found. Please make sure you passed the right file \n");
+      exit(-1);
+    }
+    const int n = vel.size();
+    bodyVelocities.resize(n);
+    assert(bodyVelocities.size() == bodiesID.size());
+#pragma omp parallel for
+    for (int i = 0; i < n; i++)
+    {
+      bodyVelocities[i].x = vel[i][0];
+      bodyVelocities[i].y = vel[i][1];
+      bodyVelocities[i].z = vel[i][2];
+    }
+  }
+
+  {
+    typedef float[2] vec2;
+    Bonsai::DataType<vec2> rhoh("RHOH");
+    if (out.read.(rhoh))
+    {
+      if (myRank == 0)
+      {
+        fprintf(stderr , " -- No RHOH data is found \n");
+      }
+      const int n = rhoh.size();
+      rhohList.resize(n);
+      assert(rhohList.size() == bodiesID.size());
+#pragma omp parallel for
+      for (int i = 0; i < n; i++)
+      {
+        rhohList[i].x = rhoh[i][0];
+        rhohList[i].y = rhoh[i][1];
+      }
+    }
+    else if (myRank == 0)
+    {
+      fprintf(stderr , " -- No RHOH data is found \n");
+    }
+  }
+
+}
+                                                                                                                                                                                                                        const bool restart)
+
+
 
 namespace BonsaiIO
 {
@@ -90,7 +303,4 @@ namespace BonsaiIO
           dtOpenMax);
     }
   }
-
-
-  void IDType_t :: write(
 }
--- IO/BonsaiIO.h
+++ IO/BonsaiIO.h
@@ -2,24 +2,225 @@
 
 namespace BonsaiIO
 {
-  typedef float  fp32_t;
-  typedef double fp64_t;
+  typedef long long int long_t;
 
-  /*********** Bonsai Header *************/
+  /*********** Header ******************/
+  class Header
+  {
+    private:
+      const std::string versionString;
+      double time;
+
+    public:
+    Header() : versionString("V1")
+    {
+    }
+
+  };
+
+  
+  /*********** Data types *************/
+
+  class DataTypeBase
+  {
+    private:
+      std::string name;
+      long_t n;
+      int   elementSize;
+    public:
+      virtual void DataTypeBase(const std::string _name, const int n) : name(_name), n(_n) {};
+      virtual void ~DataTypeBase() {};
+      const std::string& typeName() const {return name;}
+  };
+
+
+  template<typename T>
+    class DataType : public DataTypeBase
+  {
+    private:
+      std::vector<T> data;
+    public:
+      virtual void DataType(std::string _name, const int n = 0) : DataTypeBase(_name, _n)
+    {
+      if (n > 0)
+        data.resize(n);
+      dataSize = sizeof(T);
+    }
+      const T& operator[](const size_t i) const { return data[i]; }
+      T operator[](const size_t i) const { return data[i]; }
+  };
+
+  /*********** Core reader/writer *************/
+
+  enum IOTYPE 
+  {
+    FREAD, FWRITE,         /* one file per MPI proc, uses default fread/fwrite */
+    READMPI, WRITEMPI      /* use MPI-IO */
+  }
 
   class Core
   {
-    std::vector<std::pair<uint64_t,DataStructBase*>> data;
-  }
+    private:
+      const int myRank;
+      const int nRank;
+      const MPIComm &comm;
+      const IOTYPE iotype;
+      const std::string fileName;
 
-  class BonsaiHeader
+      MPI_File fh;
+      MPI_Status mpi;
+
+      Header header;
+      
+
+    public:
+        Core(const int _myRank,
+            const int _nRank,
+            const MPI_Comm &_comm,
+            const IOTYPE _iotype,
+            const std::string _fileName) :
+          write(false)
+          myRank(_myRank),
+          nRank(_nRank),
+          comm(_comm),
+          iotype(_iotype),
+          fileName(_fileName)
+    {
+      int file_open_error;
+      switch (iotype)
+      {
+        case WRITEMPI:
+          MPI_File_open(
+              comm,
+              fileName,c_str(),
+              MPI_MODE_CREATE | MPI_MODE_RDWR,
+              MPI_INFO_NULL,
+              &fn);
+          break;
+        case READMPI:
+          file_open_error = MPI_File_open(
+              comm,
+              fileName,c_str(),
+              MPI_MODE_RDONLY,
+              MPI_INFO_NUL,
+              &fh);
+          if (file_open_error != MPI_SUCCESS)
+          {
+            char error_string[256];
+            int length_of_error_string, error_class;
+
+            MPI_Error_class(file_open_error, &error_class);
+            MPI_Error_string(error_class, error_string, &length_of_error_string);
+            printf("%3d: %s\n", my_rank, error_string);
+
+            MPI_Error_string(file_open_error, error_string, &length_of_error_string);
+            printf("%3d: %s\n", my_rank, error_string);
+
+            MPI_Abort(MPI_COMM_WORLD, file_open_error);
+          }
+          break;
+      }
+    }
+
+
+        bool readMPIIO(DataTypeBase &data)
+        {
+          assert(isRead());
+          if (!header.seek(data.getName()))
+            return false;
+
+          const size_t nBytesGbl = header.getNBytes();
+
+
+          MPI_Offset dataOffset = header.getOffset();
+
+          const uint64_t nBytesPerRank = (nBytesGlb - 1 + nRank) / nRank;
+          const uint64_t beg = myRank * nBytesPerRank;
+          const uint64_t end = std::min(beg + nBytesPerRank, nBytesGlb);
+          
+          data.allocate(nBytesPerRank);
+          
+          uint64_t nBytes = end - beg;
+          const uint64_t nMaxPerRead = (1U << 31) - 1;
+          while (nBytes > 0)
+          {
+            const int count = static_cast<int>(std::min(nBytes, nMaxPerWrite));
+            assert(count > 0);
+            MPI_File_read(
+                fh,
+                data.getDataPtr(),
+                count, 
+                MPI_BYTE, 
+                &status);
+            int read;
+            MPI_Get_count(&status, MPI_INT, &read);
+            assert(count == read);
+            nBytes -= nMaxPerRead;
+          }
+          return true;
+        }
+
+        bool writeMPIIO(const DataTypeBase &data)
+        {
+          assert(isWrite());
+          const double tWrite = MPI_Wtime();
+
+
+          uint64_t nBytesLoc = data.getN() * data.unitSize();
+          uint64_t nBytesGlb;
+          
+
+          MPI_Allreduce(&nBytesLoc, &nBytesGlb, 1, MPI_LONG_LONG, MPI_SUM, comm);
+          header.add(data.getName(), nBytesGlb);
+
+          const uint64_t nBytesPerRank = (nBytesGlb  - 1 + nRank) / nRank;
+          const uint64_t beg = myRank * nBytesPerRank;
+          const uint64_t end = std::min(beg + nBytesPerRank, nBytesGlb);
+
+          MPI_Offset myOffset = headerOffset + dataOffset + beg;
+          MPI_File_seek(fh, myOffset, MPI_SEEK_SET);
+
+          uint64_t nBytes = end - beg;
+          const uint64_t nMaxPerWrite = (1U << 31) - 1;
+          while (nBytes > 0)
+          {
+            const int count = static_cast<int>(std::min(nBytes, nMaxPerWrite));
+            assert(count > 0);
+            MPI_File_write(
+                fh,
+                data.getDataPtr(),
+                count, 
+                MPI_BYTE, 
+                &status);
+            int written;
+            MPI_Get_count(&status, MPI_INT, &written);
+            assert(count == written);
+            nBytes -= nMaxPerWrite;
+          }
+
+          dataOffset += nBytesGlb;
+
+          dtWrite += MPI_Wtime() - tWrite;
+          return true;
+        }
+
+
+        void close()
+        {
+          /* write header */
+          MPI_File_close(&fh);
+        }
+
+  };
+
+  class Header
   {
     const char versionString[256];
     fp64_t time;
     fp64_t dummySpace[256];
     std::vector<std::pair<uint64_t,uint64_t>> typeInfo
   };
-  
+
   /*********** Bonsai Data Types *************/
 
   enum TypeName 
@@ -37,10 +238,10 @@ namespace BonsaiIO
     public:
       virtual TypeName getTypeName() const = 0;
   };
-  
+
   /*********** Bonsai Data Type Derived Classes  *************/
 
-  class IDType_t : public DataStructBase
+  class IDType : public DataStructBase
   {
     private:
       uint64_t _IDTypePacked;
@@ -97,7 +298,7 @@ namespace BonsaiIO
       fp32_t  _h() const { return _h; }
       fp32_t& _h()       { return _h; }
   };
-  
+
   class GasData : public DensityH
   {
     private:
