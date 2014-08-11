7e185848303b3d01098fa413f12fba8007138fa3
Evghenii <egaburov@dds.nl>
2014-08-11 07:32:18 +0200

    +1

--- render/splotchGL/renderloop.h
+++
@@ -1,9 +0,0 @@
-#ifndef _RENDERLOOP_H_
-#define _RENDERLOOP_H_
-
-#include "RendererData.h"
-
-void initAppRenderer(int argc, char** argv, 
-                     RendererData &data);
-#define PARTICLESRENDERER
-#endif // _RENDERLOOP_H_
