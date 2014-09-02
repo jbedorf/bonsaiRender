98a3f2caea1d0f9ac60384d89724c1aca0abd9c3
evghenii <egaburov@dds.nl>
2014-09-02 09:02:50 +0200

    converts

--- IO/cvt_jamie2bonsai.cpp
+++ IO/cvt_jamie2bonsai.cpp
@@ -1,102 +1,5 @@
 #include "BonsaiIO.h"
 
-#if 0
-template<typename IO>
-static double writeDM(ReadTipsy &data, IO &out)
-{
-  double dtWrite = 0;
-  const int pCount  = data.firstID.size();
-  /* write IDs */
-  {
-    BonsaiIO::DataType<IDType> ID("DM:IDType", pCount);
-    for (int i = 0; i< pCount; i++)
-    {
-      ID[i].setID(data.firstID[i]);
-      ID[i].setType(0);
-    }
-    double t0 = MPI_Wtime();
-    out.write(ID);
-    dtWrite += MPI_Wtime() - t0;
-  }
-  
-  /* write pos */
-  {
-    BonsaiIO::DataType<ReadTipsy::real4> pos("DM:POS:real4",pCount);
-    for (int i = 0; i< pCount; i++)
-      pos[i] = data.firstPos[i];
-    double t0 = MPI_Wtime();
-    out.write(pos);
-    dtWrite += MPI_Wtime() - t0;
-  }
-    
-  /* write vel */
-  {
-    typedef float vec3[3];
-    BonsaiIO::DataType<vec3> vel("DM:VEL:float[3]",pCount);
-    for (int i = 0; i< pCount; i++)
-    {
-      vel[i][0] = data.firstVel[i].x;
-      vel[i][1] = data.firstVel[i].y;
-      vel[i][2] = data.firstVel[i].z;
-    }
-    double t0 = MPI_Wtime();
-    out.write(vel);
-    dtWrite += MPI_Wtime() - t0;
-  }
-
-  return dtWrite;
-}
-
-template<typename IO>
-static double writeStars(ReadTipsy &data, IO &out)
-{
-  double dtWrite = 0;
-
-  const int pCount  = data.secondID.size();
-
-  /* write IDs */
-  {
-    BonsaiIO::DataType<IDType> ID("Stars:IDType", pCount);
-    for (int i = 0; i< pCount; i++)
-    {
-      ID[i].setID(data.secondID[i]);
-      ID[i].setType(1);
-    }
-    double t0 = MPI_Wtime();
-    out.write(ID);
-    dtWrite += MPI_Wtime() - t0;
-  }
-    
-  /* write pos */
-  {
-    BonsaiIO::DataType<ReadTipsy::real4> pos("Stars:POS:real4",pCount);
-    for (int i = 0; i< pCount; i++)
-      pos[i] = data.secondPos[i];
-    double t0 = MPI_Wtime();
-    out.write(pos);
-    dtWrite += MPI_Wtime() - t0;
-  }
-    
-  /* write vel */
-  {
-    typedef float vec3[3];
-    BonsaiIO::DataType<vec3> vel("Stars:VEL:float[3]",pCount);
-    for (int i = 0; i< pCount; i++)
-    {
-      vel[i][0] = data.secondVel[i].x;
-      vel[i][1] = data.secondVel[i].y;
-      vel[i][2] = data.secondVel[i].z;
-    }
-    double t0 = MPI_Wtime();
-    out.write(vel);
-    dtWrite += MPI_Wtime() - t0;
-  }
-
-  return dtWrite;
-}
-#endif
-
-
 int main(int argc, char * argv[])
 {
   MPI_Comm comm = MPI_COMM_WORLD;
@@ -123,116 +26,96 @@ int main(int argc, char * argv[])
   const std::string inputFn(argv[1]);
   const std::string outputFn(argv[2]);
 
+  FILE *fin = fopen(inputFn.c_str(), "rb");
+  assert(fin);
+
+  struct __attribute__((__packed__)) header_t
   {
-    FILE *fin = fopen(inputFn.c_str(), "rb");
-    assert(fin);
+    int ntot;
+    int nnopt;
+    double hmin;
+    double hmax;
+    double sep0;
+    double tf;
+    double dtout;
+    int nout;
+    int nit;
+    double t;
+    int anv;
+    double alpha;
+    double beta;
+    double tskip;
+    int ngr;
+    int nrelax;
+    double trelax;
+    double dt;
+    double omega2;
+  };
 
-    struct __attribute__((__packed__)) header_t
-    {
-      int ntot;
-      int nnopt;
-      double hmin;
-      double hmax;
-      double sep0;
-      double tf;
-      double dtout;
-      int nout;
-      int nit;
-      double t;
-      int anv;
-      double alpha;
-      double beta;
-      double tskip;
-      int ngr;
-      int nrelax;
-      double trelax;
-      double dt;
-      double omega2;
-    };
+  int idum; 
+  header_t h;
+  fread(&idum,  sizeof(int), 1, fin); 
+  assert(idum == (int)sizeof(header_t));
 
-    int idum; 
-    header_t h;
-    fread(&idum /* recsz */,  sizeof(int), 1, fin); assert(idum == (int)sizeof(header_t));
-    fread(&h, sizeof(header_t), 1, fin);
-    fread(&idum /* recsz */,  sizeof(int), 1, fin); assert(idum == (int)sizeof(header_t));
+  fread(&h, sizeof(header_t), 1, fin);
 
-    fprintf(stderr, "ntot= %d  t= %g \n", h.ntot, h.t);
+  fread(&idum,  sizeof(int), 1, fin); 
+  assert(idum == (int)sizeof(header_t));
+
+  fprintf(stderr, "ntot= %d  t= %g \n", h.ntot, h.t);
+
+  struct __attribute__((__packed__)) sph_t
+  {
+    double x,y,z;
+    double am,hp,rho;
+    double vx,vy,vz;
+    double vxdot,vydot,vzdot;
+    double u,udot;
+    double grpot, mmu;
+    int cc;
+    double divv;
+  };
+
+  std::vector<sph_t> data(h.ntot);
+
+  for (int i = 0; i < h.ntot; i++)
+  {
+    fread(&idum,  sizeof(int), 1, fin); 
+    assert(idum == (int)sizeof(sph_t));
+
+    fread(&data[i], sizeof(sph_t), 1, fin);
+
+    fread(&idum,  sizeof(int), 1, fin); 
+    assert(idum == (int)sizeof(sph_t));
   }
 
+  fread(&idum,  sizeof(int), 1, fin); 
+  assert(idum == (int)sizeof(int));
 
-#if 0
-  ReadTipsy data(
-      baseName, 
-      rank, nranks,
-      nDomains, 
-      reduceFactorFirst,
-      reduceFactorSecond);
+  fread(&idum, sizeof(int), 1, fin);
+  assert(idum == h.ntot);
 
-  long long nFirstLocal = data.firstID.size();
-  long long nSecondLocal = data.secondID.size();
+  fread(&idum,  sizeof(int), 1, fin); 
+  assert(idum == (int)sizeof(int));
+   
 
-  long long nFirst, nSecond;
-  MPI_Allreduce(&nFirstLocal, &nFirst, 1, MPI_LONG, MPI_SUM, comm);
-  MPI_Allreduce(&nSecondLocal, &nSecond, 1, MPI_LONG, MPI_SUM, comm);
+  const int ntot = h.ntot;
+  BonsaiIO::Core out(rank, nranks, comm, BonsaiIO::WRITE, outputFn);
 
-  if (rank == 0)
+  /* write metadata */
   {
-    fprintf(stderr, " nFirst = %lld \n", nFirst);
-    fprintf(stderr, " nSecond= %lld \n", nSecond);
-    fprintf(stderr, " nTotal= %lld \n", nFirst + nSecond);
+    BonsaiIO::DataType<header_t> el("SPH:header:jamieHeader_t", 1);
+    el[0] = h;
+    out.write(el);
   }
 
-  const double tAll = MPI_Wtime();
+  /* write pos */
   {
-    const double tOpen = MPI_Wtime();
-    BonsaiIO::Core out(rank, nranks, comm, BonsaiIO::WRITE, outputName);
-    double dtOpenLoc = MPI_Wtime() - tOpen;
-    double dtOpenGlb;
-    MPI_Allreduce(&dtOpenLoc, &dtOpenGlb, 1, MPI_DOUBLE, MPI_MAX,comm);
-    if (rank == 0)
-      fprintf(stderr, "open file in %g sec \n", dtOpenGlb);
-
-    double dtWrite = 0;
-
-    
-    if (rank == 0)
-      fprintf(stderr, " write DM  \n");
-    MPI_Barrier(comm);
-    dtWrite += writeDM(data,out);
-
-    if (rank == 0)
-      fprintf(stderr, " write Stars\n");
-    MPI_Barrier(comm);
-    dtWrite += writeStars(data,out);
-
-
-    double dtWriteGlb;
-    MPI_Allreduce(&dtWrite, &dtWriteGlb, 1, MPI_DOUBLE, MPI_MAX,comm);
-    if (rank == 0)
-      fprintf(stderr, "write file in %g sec \n", dtWriteGlb);
-
-    const double tClose = MPI_Wtime();
-    out.close();
-    double dtCloseLoc = MPI_Wtime() - tClose;
-    double dtCloseGlb;
-    MPI_Allreduce(&dtCloseLoc, &dtCloseGlb, 1, MPI_DOUBLE, MPI_MAX,comm);
-    if (rank == 0)
-      fprintf(stderr, "close time in %g sec \n", dtCloseGlb);
-
-    if (rank == 0)
-    {
-      out.getHeader().printFields();
-      fprintf(stderr, " Bandwidth= %g MB/s\n", out.computeBandwidth()/1e6);
-    }
+    BonsaiIO::DataType<sph_t> el("SPH:data:jamieData_t", ntot);
+    for (int i = 0; i < ntot; i++)
+      el[i] = data[i];
+    out.write(el);
   }
-  double dtAllLoc = MPI_Wtime() - tAll;
-  double dtAllGlb;
-  MPI_Allreduce(&dtAllLoc, &dtAllGlb, 1, MPI_DOUBLE, MPI_MAX,comm);
-  if (rank == 0)
-    fprintf(stderr, "All operations done in   %g sec \n", dtAllGlb);
-
-#endif
-
 
 
   MPI_Finalize();
